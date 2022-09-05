abd79ee9ccf2f671bb7d3df8644af3fecd686b92
Manisha Basra <manisha.4918@gmail.com>
2022-09-05 14:06:40 +0530

    currying function

--- Questions.js
+++ Questions.js
@@ -1,4 +1,14 @@
-/* Question : (a) Create the polyfill for the filter and then (b) use that filter method to remove the duplicate from the given string. */
+/* 
+
+Question 1: (a) Create the polyfill for the filter and then (b) use that filter method to remove the duplicate from the given string. 
+
+Array.prototype.myFilter = // your own implemenation of the filter method
+removeDuplicate('Manisha Basra JS coder');
+
+*/
+
+
+
 
 // polyfill code for filter array method
 function myFilter (fn) {
@@ -13,7 +23,7 @@ function myFilter (fn) {
   }
 
 // remove duplicate using call and above myFilter method
-function removeDuplicateCharacters(string) {
+function removeDuplicate(string) {
   const strArray = string.split('');
   let result = myFilter.call(strArray, (item, index, arr) => {
     return item === ' ' || arr.indexOf(item) == index;
@@ -21,7 +31,44 @@ function removeDuplicateCharacters(string) {
   return result;  //Manish Br JS code
 }
 
-console.log(removeDuplicateCharacters('Manisha Basra JS coder'));
+console.log(removeDuplicate('Manisha Basra JS coder'));
 
 
 /* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+/*
+Question 2: Complete the implementation of the logger function.
+
+
+const logger = fn => {
+  // .... ?
+}
+
+const add = (a, b) => a + b;
+const reverseStr = str => str.split("").reverse().join('')
+
+const res1 = logger(add)(1, 2)
+// args are: 1, 2
+// result is: 3
+
+const res2 = logger(reverseStr)('jscodelover')
+// args are: jscodelover
+// result is: revoledocsj
+
+*/
+
+const logger = fn => {
+  return (...args) => {
+    const result = fn(...args);
+    console.log(`args are: ${args}`)
+    console.log(`result is: ${result}`)
+  }
+}
+
+
+
+/* ----------------------------------------------------------------------------------------------------------------------------------------------------- */
+
+
+
